c2093afdec05cb9e7265382339831e00e1f668a1
pradeepkumargopi <Pradeep.Kumar.Gopi@gds.ey.com>
2025-01-24 14:25:27 +0530

    Create clonedb.sh

---
+++ migrations/clonedb.sh
@@ -0,0 +1,16 @@
+# simple_integration.sh script that reads workflow.conf file and execute it.
+#!/bin/bash
+# author: Marcel Castro
+set -e
+print_log () {
+    printf "[`date +'%d/%m/%Y %H:%M:%S'`] [$1] $2\n"
+}
+export -f print_log
+
+run_workflow () {
+    print_log "INFO" "Running workflow"
+        ~/snowflake/snowsql -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE -f ${GITHUB_WORKSPACE}/${clone_sql_file_name}
+}
+
+## running workflow
+run_workflow
