f829f881e1ccd1c5194c5c66ea634cd43410bc07
pradeepkumargopi <Pradeep.Kumar.Gopi@gds.ey.com>
2025-01-24 15:23:34 +0530

    Update clonedb.sh

--- migrations/clonedb.sh
+++ migrations/clonedb.sh
@@ -6,9 +6,10 @@ print_log () {
     printf "[`date +'%d/%m/%Y %H:%M:%S'`] [$1] $2\n"
 }
 export -f print_log
-export SNOWSQL_PWD=Training@123
+
 run_workflow () {
     print_log "INFO" "Running workflow"
+    export SNOWSQL_PWD=Training@123
         ~/snowflake/snowsql -a io63122.central-india.azure -u pradeep -o log_level=DEBUG -q "create database training_new_bkp clone training_new;"
 }
 
